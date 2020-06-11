7d7ba6210bf579c68e128915b2a4411bb9a530df
Baig501st <66305317+Baig501st@users.noreply.github.com>
2020-06-11 14:33:00 +0800

    Create view.php

---
+++ view.php
@@ -0,0 +1,14 @@
+<?php
+
+//connection string
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "A2663405d", "Database" => "dbtp042428", 
+"LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:ddsctp042428.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if(!$conn)
+{
+  die("Error connection: ".sqlsrv_errors())
+}
+ echo "Connection Success: connected!";
+?>
